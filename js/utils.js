16fae739ece7a285a8bafad4bd2275c32717171f
anonion <ananonion@gmail.com>
2023-12-29 10:06:51 -0700

    formatting

--- js/utils.js
+++ js/utils.js
@@ -1,25 +1,25 @@
 export function updateRedirects() {
-    chrome.storage.local.get("config", function (URL) {
-      fetch(URL.config)
-        .then((res) => res.json())
-        .then((res) => {
-          chrome.storage.local
-            .set({
-              redirects: res,
-            })
-            .catch((error) => {
-              console.log(error);
-            });
-        })
-        .catch((error) => {
-          console.log(error);
-        });
-    });
-  }
+  chrome.storage.local.get("config", function (URL) {
+    fetch(URL.config)
+      .then((res) => res.json())
+      .then((res) => {
+        chrome.storage.local
+          .set({
+            redirects: res,
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  });
+}
 
 export function setDefaultConfig() {
   chrome.storage.local.set({
     config:
     "https://raw.githubusercontent.com/davegallant/rfd-redirect-stripper/main/redirects.json",
-});
+  });
 }
