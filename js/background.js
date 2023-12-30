48c978434fabd3a07313f83e99d0b88816220c14
anonion <ananonion@gmail.com>
2023-12-30 14:34:44 -0700

    add onStartup listener to ensure alarm is created

--- js/background.js
+++ js/background.js
@@ -1,14 +1,25 @@
 import { updateRedirects, setDefaultConfig } from "../js/utils.js"
 
-chrome.alarms.get('update-redirects', alarm => {
-  if (!alarm)
-    chrome.alarms.create('update-redirects', { delayInMinutes: 60 });
-});
+function setAlarm() {
+  chrome.alarms.get('update-redirects', alarm => {
+    if (!alarm) {
+      chrome.alarms.create('update-redirects', { periodInMinutes: 1 });
+    }
+  });
+}
+
 chrome.alarms.onAlarm.addListener(() => {
+  console.log("alarm fired");
   updateRedirects();
 });
 
 chrome.runtime.onInstalled.addListener(() => {
   setDefaultConfig();
   updateRedirects();
+  setAlarm();
+});
+
+//Ensure alarm is created
+chrome.runtime.onStartup.addListener(() => {
+  setAlarm();
 });
